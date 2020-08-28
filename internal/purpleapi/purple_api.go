fc32fbc0d7f24f959e5dfae697eff378ee853702
MrFlynn <nick@pleatsikas.me>
2020-08-27 21:33:36 -0700

    Adding Purple Air API package.
    This package is designed to download and parse all sensor data from
    Purple Air's API. Data is packaged in structs that can be easily
    serialized into a database format.

---
+++ internal/purpleapi/purple_api.go
@@ -0,0 +1,91 @@
+package purpleapi
+
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+const apiURL = "https://www.purpleair.com/json"
+
+// Enable encoding/json compat.
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+// Sensor location enum.
+type Location int
+
+const (
+	Unknown Location = iota
+	Outside
+	Inside
+)
+
+func toLocation(s string) Location {
+	switch strings.TrimSpace(strings.ToLower(s)) {
+	case "outside":
+		return Outside
+	case "inside":
+		return Inside
+	default:
+		return Unknown
+	}
+}
+
+type outerResponse struct {
+	Results []Response `json:"results"`
+}
+
+// Response encodes the relevant data from the Purple Air api.
+type Response struct {
+	ID        int      `json:"ID" db:"primary_key"`
+	ParentID  int      `json:"ParentID,omitempty" db:"append_only"`
+	Location  Location `json:"DEVICE_LOCATIONTYPE,omitempty" db:"ignore"`
+	Latitude  float32  `json:"Lat" db:"value,geo"`
+	Longitude float32  `json:"Lon" db:"value,geo"`
+	PM25      float32  `json:"PM2_5Value,string" db:"value,quality"`
+}
+
+// UnmarshalJSON implements a custom unmarshaller for the Response type.
+// This is to transform the location string into the proper enum type.
+func (r *Response) UnmarshalJSON(data []byte) error {
+	type Alias Response
+	aux := &struct {
+		Location string `json:"DEVICE_LOCATIONTYPE,omitempty"`
+		*Alias
+	}{
+		Alias: (*Alias)(r),
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	r.Location = toLocation(aux.Location)
+
+	return nil
+}
+
+func decode(r io.ReadCloser) ([]Response, error) {
+	apiData := &outerResponse{}
+
+	err := json.NewDecoder(r).Decode(apiData)
+	if err != nil {
+		return []Response{}, err
+	}
+
+	return apiData.Results, nil
+}
+
+// Get grabs data from Purple Air's API and returns a list of current measurements.
+func Get() ([]Response, error) {
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return []Response{}, err
+	}
+
+	defer resp.Body.Close()
+
+	return decode(resp.Body)
+}
---
+++ internal/purpleapi/purple_api_test.go
@@ -0,0 +1,89 @@
+package purpleapi
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+// Example data taken from Purple Air's API.
+var (
+	expectedResponse = []Response{
+		{
+			ID:        14633,
+			Location:  Outside,
+			Latitude:  37.275561,
+			Longitude: -121.964134,
+			PM25:      24.68,
+		},
+		{
+			ID:        14634,
+			ParentID:  14633,
+			Location:  Unknown,
+			Latitude:  37.275561,
+			Longitude: -121.964134,
+			PM25:      25.12,
+		},
+	}
+	testData = `{
+		"mapVersion":"0.17",
+		"baseVersion":"7",
+		"mapVersionString":"",
+		"results":[
+			{
+				"ID":14633,
+				"Label":" Hazelwood canary ",
+				"DEVICE_LOCATIONTYPE":"outside",
+				"THINGSPEAK_PRIMARY_ID":"559921",
+				"THINGSPEAK_PRIMARY_ID_READ_KEY":"CU4BQZZ38WO5UJ4C",
+				"THINGSPEAK_SECONDARY_ID":"559922",
+				"THINGSPEAK_SECONDARY_ID_READ_KEY":"D0YNZ1LM59LL49VQ",
+				"Lat":37.275561,
+				"Lon":-121.964134,
+				"PM2_5Value":"24.68",
+				"LastSeen":1598334840,
+				"Type":"PMS5003+PMS5003+BME280",
+				"Hidden":"false",
+				"isOwner":0,
+				"humidity":"50",
+				"temp_f":"79",
+				"pressure":"1003.86",
+				"AGE":0,
+				"Stats":"{\"v\":24.68,\"v1\":23.86,\"v2\":23.55,\"v3\":28.07,\"v4\":59.12,\"v5\":61.21,\"v6\":25.77,\"pm\":24.68,\"lastModified\":1598334840227,\"timeSinceModified\":119989}"
+			},
+			{
+				"ID":14634,
+				"ParentID":14633,
+				"Label":" Hazelwood canary  B",
+				"THINGSPEAK_PRIMARY_ID":"559923",
+				"THINGSPEAK_PRIMARY_ID_READ_KEY":"DULWDNCI9M6PCIPC",
+				"THINGSPEAK_SECONDARY_ID":"559924",
+				"THINGSPEAK_SECONDARY_ID_READ_KEY":"EY2CNMYRUZHDW1AL",
+				"Lat":37.275561,
+				"Lon":-121.964134,
+				"PM2_5Value":"25.12",
+				"LastSeen":1598334840,
+				"Hidden":"false",
+				"Flag":1,
+				"isOwner":0,
+				"AGE":0,
+				"Stats":"{\"v\":25.12,\"v1\":23.7,\"v2\":23.24,\"v3\":27.63,\"v4\":58.6,\"v5\":59.65,\"v6\":21.82,\"pm\":25.12,\"lastModified\":1598334840228,\"timeSinceModified\":119990}"
+			}
+		]
+	}`
+)
+
+func TestDecode(t *testing.T) {
+	reader := ioutil.NopCloser(strings.NewReader(testData))
+
+	resp, err := decode(reader)
+	if err != nil {
+		t.Errorf("Got error during decode: %s", err)
+	}
+
+	if !cmp.Equal(resp, expectedResponse) {
+		t.Errorf("Expected: %+v\nGot: %+v\n", expectedResponse, resp)
+	}
+}
