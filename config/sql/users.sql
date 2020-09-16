15b1054f0109f3b41edd34ecd343b832607df42a
MrFlynn <nick@pleatsikas.me>
2020-09-15 23:47:09 -0700

    Adding SQL database schema.
    This database will store long-term records like web push notification
    subscription information and other user preferences.

---
+++ config/sql/users.sql
@@ -0,0 +1,11 @@
+drop table if exists users;
+
+create table users (
+    id serial not null primary key,
+    push_url text not null,
+    private_key text not null,
+    public_key text not null,
+    longitude double precision not null,
+    latitude double precision not null,
+    threshold double precision not null
+);
