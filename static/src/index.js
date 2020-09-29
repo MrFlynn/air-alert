058c008de4a053688458a4b4fc29c7c3bbf750e4
MrFlynn <nick@pleatsikas.me>
2020-09-28 23:16:37 -0700

    Added aqi box background changing logic.
    Background color of box changes whenever AQI goes over some predifined
    thresholds.
    
    Added margin to top of boxes so they aren't pressed against the navbar.

--- static/src/index.js
+++ static/src/index.js
@@ -1,17 +1,32 @@
 import './index.scss'
 
 if (!navigator.geolocation) {
-  console.error("geolocation not supported")
+  console.error("geolocation not supported");
 } else {
   navigator.geolocation.getCurrentPosition(l => {
-    getAQI(l.coords.latitude, l.coords.longitude)
+    getAQI(l.coords.latitude, l.coords.longitude);
   },
-  e => console.error(e))
+  e => console.error(e));
 }
 
 function getAQI(latitude, longitude) {
   fetch(`/aqi/current?lat=${latitude}&long=${longitude}`)
     .then(resp => resp.text())
-    .then(data => document.getElementById("aqi").textContent = data)
-    .catch(e => console.error(e))
+    .then(aqi => updateDisplayBox(aqi))
+    .catch(err => console.error(err));
+}
+
+function updateDisplayBox(aqi) {
+  document.getElementById("aqi-value").textContent = aqi
+
+  let aqiNum = parseFloat(aqi);
+  let color = "hsl(141, 53%, 53%)";
+
+  if (aqiNum > 50.0 && aqiNum <= 200) {
+    color = "hsl(48, 100%, 67%)"
+  } else if (aqiNum > 200.0) {
+    color = "hsl(348, 100%, 61%)"
+  }
+
+  document.getElementById("aqi-box").style.backgroundColor = color
 }
