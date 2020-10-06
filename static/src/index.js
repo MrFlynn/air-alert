23e17b1a5a689969394646338248ba484da99dab
MrFlynn <nick@pleatsikas.me>
2020-10-05 22:58:11 -0700

    Added notification subscription UI.
    Adds modal where user can select AQI threshold and subscribe to
    notifications. Also allows a user who is already subscribed to remove
    their subscription.

--- static/src/index.js
+++ static/src/index.js
@@ -1,11 +1,24 @@
 import './index.scss'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/worker.js');
+}
+
 // Get client current location.
 if (!navigator.geolocation) {
   console.error("geolocation not supported");
 } else {
   getPosition(getAQI)
 }
+
+// Register click handler to toggle modal open and closed.
+document.querySelectorAll("#toggle-modal").forEach(e => {
+  e.onclick = toggleModal;
+});
+
+// Register handler for subscribe button.
+document.getElementById('subscribe-button').onclick = subscribe;
+document.getElementById('stop-notifications').onclick = unsubscribe;
 
 function getPosition(callback) {
   navigator.geolocation.getCurrentPosition(
@@ -34,4 +47,87 @@ function updateDisplayBox(aqi) {
   }
 
   document.getElementById("aqi-box").style.backgroundColor = color
+}
+
+function toggleModal() {
+  let target = document.getElementById('subscribe-modal')
+  if (target.classList.contains('is-active')) {
+    target.classList.remove('is-active');
+  } else {
+    target.classList.add('is-active');
+    showCancelSubscriptionButton();
+  }
+}
+
+function showCancelSubscriptionButton() {
+  navigator.serviceWorker.ready.then(reg => {
+    return reg.pushManager.getSubscription().then(sub => {
+      if (sub !== null) {
+        document.getElementById('stop-notifications').style.display = 'block';
+      }
+    });
+  });
+}
+
+// Entrypoint to subscribe to notifications.
+function subscribe() {
+  navigator.serviceWorker.ready.then(reg => {
+    return reg.pushManager.getSubscription().then(async sub => {
+      if (sub) {
+        return sub;
+      }
+
+      let resp = await fetch("/subscribe/key");
+      let vapidPublicKey = await resp.arrayBuffer();
+
+      return reg.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: vapidPublicKey
+      });
+    });
+  }).then(sub => {
+    sendSubscription(sub)
+    toggleModal();
+  });
+}
+
+function sendSubscription(subscription) {
+  let threshold = parseInt(
+    document.getElementById('threshold-preferences').value, 
+    10
+  );
+
+  getPosition((lat, long) => {
+    fetch('/subscribe', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        subscription: subscription,
+        latitude: lat,
+        longitude: long,
+        threshold: threshold
+      })
+    });
+  });
+}
+
+function unsubscribe() {
+  navigator.serviceWorker.ready.then(reg => {
+    reg.pushManager.getSubscription().then(sub => {
+      fetch('/unsubscribe', {
+        method: 'delete',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          subscription: sub
+        })
+      }).then(_ => {
+        sub.unsubscribe();
+        toggleModal();
+      });
+    });
+  });
 }
